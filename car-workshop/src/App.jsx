d16fdcd09a5a1f22990725c9e71ab97ab7e31673
juinnsheng <116355442+juinnsheng@users.noreply.github.com>
2024-12-08 09:54:05 +0800

    test1

--- car-workshop/src/App.jsx
+++ car-workshop/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import LandingPage from "./Components/LandingPage/LandingPage"; 
@@ -7,8 +6,8 @@ import WhyUs from "./Components/WhyUs/WhyUs";
 import OurServices from "./Components/OurServices/OurServices";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Towing from "./Components/Towing/Towing";
-import Quote from "./Components/Quote/Quote"
-import Contact from "./Components/Contact/Contact"
+import Quote from "./Components/Quote/Quote";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   const [showLanding, setShowLanding] = useState(true);
@@ -24,22 +23,20 @@ const App = () => {
       ) : (
         <>
           <Navbar />
-          <section className="video-background">
-            <video className="intro-video" autoPlay muted loop>
-              <source src="/video/1.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-            <div className="overlay-text">From Bumps and Bruises to Showroom Shine—Let Us Heal Your Ride</div>
+          <section className="background-section">
+            <div className="overlay-text">
+              From Bumps and Bruises to Showroom Shine<br />
+              Let Us Heal Your Ride
+            </div>
           </section>
 
-          {/* Sections below the background video */}
+          {/* Sections below the background */}
           <WhyUs />
           <OurServices />
           <Testimonials />
           <Towing />
           <Quote />
           <Contact />
-
         </>
       )}
     </div>
